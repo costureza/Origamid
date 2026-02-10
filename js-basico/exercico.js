315a7daf17921e89e400fdf5a1e2576befaf6eb2
Rosa <costurezati@gmail.com>
2026-02-10 14:43:09 -0300

    .

--- js-basico/exercico.js
+++ js-basico/exercico.js
@@ -1,3 +1,39 @@
 function livro(nome, ano, autor){
-  console.log(nome.t)
+  const maiusculo = nome.toUpperCase();  
+  const calculo = 2026 - ano;
+  const frase = nome + ' por '+ autor;
+  const objeto = {
+    nome: maiusculo,
+    calculo,
+    frase,
+  };
+  return objeto;
 }
+
+const livroRetorno = livro('O senhor dos aneis', 1954, 'J.R. R. Tolkien');
+console.log(livroRetorno);
+
+// mais limpa
+function book(nome, ano, autor){
+  const objeto = {
+    nome: nome.toUpperCase(),
+    ano: 2026 - ano,
+    frase: nome + ' por '+ autor,
+  };
+  return objeto;
+}
+
+const bookRetorno = livro('O senhor dos aneis', 1954, 'J.R. R. Tolkien');
+console.log(bookRetorno);
+
+// mais limpa ainda
+function books(nome, ano, autor){
+  return {
+    nome: nome.toUpperCase(),
+    ano: 2026 - ano,
+    frase: nome + ' por '+ autor,
+  };
+}
+
+const booking = livro('O senhor dos aneis', 1954, 'J.R. R. Tolkien');
+console.log(booking);
